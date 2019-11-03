f78b51b8883ce3285d0d3f368849e75279f326d2
Ardhi <ardhifnoor@outlook.com>
2019-11-03 10:22:03 +0700

    Add entrypoint : GET albums BY ID

--- controller/albums.js
+++ controller/albums.js
@@ -2,9 +2,30 @@ let albums = {}
 
 albums.getAll = (req, res)=>{
     console.log('GET ', req.url)
-    const result = req.db.get('albums').value()
+    
+    const result = req.db
+        .get('albums')
+        .value()
+    
     console.log('Result : ', JSON.stringify(result))
     res.send(result)
+}
+
+albums.getByID = (req, res)=>{
+    console.log('GET ', req.url)
+    
+    const result = req.db
+        .get('albums')
+        .find({ id : req.params.id })
+        .value()
+    
+    console.log('Result : ', JSON.stringify(result))
+    
+    if(result){
+        res.send(result)
+    } else {
+        res.status(404).send('<pre> 404 Not Found! </pre>')
+    }
 }
 
 module.exports = albums
--- routes/v1.js
+++ routes/v1.js
@@ -4,6 +4,8 @@ const Router    = express.Router()
 // Import Controller
 const albumsController  = require('../controller/albums')
 
+// Use Controller for routing
 Router.get('/albums', albumsController.getAll)
+Router.get('/albums/:id', albumsController.getByID)
 
 module.exports = Router
